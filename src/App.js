bdc071d9f29b63634d3f882d17051e736f053a38
Daniel Hootini <hootinid@gmail.com>
2024-09-03 13:42:53 -1000

    feat: Add routing for Queue, AddToQueue, TutorDashboard, and StudentView components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Queue from './components/Queue';
+import AddToQueue from './components/AddToQueue';
+import TutorDashboard from './components/TutorDashboard';
+import StudentView from './components/StudentView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Queue />} />
+        <Route path="/add" element={<AddToQueue />} />
+        <Route path="/tutor" element={<TutorDashboard />} />
+        <Route path="/student" element={<StudentView />} />
+      </Routes>
+    </Router>
   );
 }
 
