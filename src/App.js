9d6b8c34ed4b96d1e7ec8fcf460442eeac073b86
Daniel Hootini <hootinid@gmail.com>
2024-09-03 14:12:38 -1000

    feat: Add About, Privacy, Header, and Footer components, and update routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Queue from './components/Queue';
 import AddToQueue from './components/AddToQueue';
 import TutorDashboard from './components/TutorDashboard';
 import StudentView from './components/StudentView';
+import About from './components/About';
+import Privacy from './components/Privacy';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Queue />} />
-        <Route path="/add" element={<AddToQueue />} />
-        <Route path="/tutor" element={<TutorDashboard />} />
-        <Route path="/student" element={<StudentView />} />
-      </Routes>
+    <Router> {/* Ensure the entire app is wrapped in Router */}
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Queue />} />
+          <Route path="/add" element={<AddToQueue />} />
+          <Route path="/tutor" element={<TutorDashboard />} />
+          <Route path="/student" element={<StudentView />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="*" element={<h1>Page Not Found</h1>} /> {/* Fallback route */}
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
