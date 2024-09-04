c8a9e167ddf6154b8fbe66ce49a2edb9dda8d831
Daniel Hootini <hootinid@gmail.com>
2024-09-03 14:12:12 -1000

    feat: Add Privacy component

---
+++ src/components/Privacy.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Privacy() {
+  return (
+    <div style={contentStyle}>
+      <h1>Privacy Policy</h1>
+      <p>
+        At gettutoring.online, accessible from http://cis.gettutoring.online, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by gettutoring.online and how we use it.
+      </p>
+    </div>
+  );
+}
+
+const contentStyle = {
+  padding: '20px',
+  textAlign: 'center',
+};
+
+export default Privacy;
