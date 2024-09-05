ba536569964c54b6e41a2f8c3f4fa7deda17a30f
Daniel Hootini <hootinid@gmail.com>
2024-09-05 13:50:35 -1000

    refactor: Add Queue component

---
+++ src/components/Queue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Queue.css';
+
+const Queue = () => {
+    return (
+        <div className="container">
+            <h1>Queue</h1>
+        </div>
+    );
+}
+
+export default Queue;
