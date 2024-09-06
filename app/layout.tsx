4cc71f5987e5eb97f5a64366a3ecbf881d12fa00
DanielH987 <hootinid@go.byuh.edu>
2024-09-05 19:07:52 -1000

    chore: Update layout component and add Footer
    This commit updates the layout component by adding the Footer component for website navigation. The Footer component will be displayed at the bottom of the page, providing links and additional information to the users. This change improves the overall navigation and user experience of the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function RootLayout({
   children,
@@ -8,9 +9,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen flex flex-col">
         <Header />
-        {children}
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </body>
     </html>
   );
