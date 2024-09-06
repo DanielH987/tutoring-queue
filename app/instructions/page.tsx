bd4ce2f67dae4d69d08374964d7ad0204aa4f541
DanielH987 <hootinid@go.byuh.edu>
2024-09-05 19:21:08 -1000

    feat: Update InstructionsPage component
    This commit updates the InstructionsPage component to provide clearer instructions on how to use the system. The content has been revised to include information on how to get help from tutors, both in the lab and on Microsoft Teams. Additionally, a note has been added to inform users about the possibility of waiting in line for assistance. This change enhances the user experience by improving the clarity and usability of the Instructions page.

--- app/instructions/page.tsx
+++ app/instructions/page.tsx
@@ -1,10 +1,14 @@
 const InstructionsPage = () => {
     return (
-        <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Instructions</h1>
-        <p>Here are the instructions on how to use the system.</p>
-        {/* Add more instructions content here */}
-        </div>
+      <div className="p-6 text-left max-w-screen-lg mx-auto">
+        <h1 className="text-3xl font-bold mb-4 mt-4">Instructions</h1>
+        <p className="mb-4">
+          To get help from the tutors, fill out the form on the main page. If you are in the lab, enter your desk number. If you want help on Microsoft Teams, enter your email.
+        </p>
+        <p>
+          Also, some tutors might not be able to help for a certain class. So, you might see some people later in &quot;line&quot; get help before you.
+        </p>
+      </div>
     );
 };
 
