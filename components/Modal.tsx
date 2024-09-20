ee981283cb606d0c832ecc03cd5fd50671a7c3c8
Daniel Hootini <hootinid@gmail.com>
2024-09-20 09:44:09 -1000

    Refactor Modal component to add dark mode support for background color

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -20,7 +20,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children, size = 
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className={`bg-white rounded-lg shadow-lg p-6 relative w-full ${sizeClasses[size]} mx-4`}>
+      <div className={`bg-white rounded-lg shadow-lg p-6 relative w-full ${sizeClasses[size]} mx-4 dark:bg-gray-800`}>
         {/* Modal Header */}
         {title && (
           <div className="flex justify-between items-center mb-4">
